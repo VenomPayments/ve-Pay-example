3545c3331a0817e61c991ae3ebb9fc57fe818074
Ruslan Galimov <rg@warp.is>
2023-06-09 16:35:18 +0000

    init

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Search from "./SearchForm";
+import Logo from "./Logo";
+import HeaderCart from "./HeaderCart";
+import { ProviderRpcClient } from "everscale-inpage-provider";
+
+const Header: React.FC =  () => {
+
+return (
+  <header className="header">
+    <div className="header__row">
+      <Logo className="header__logo" />
+      <HeaderCart />
+    </div>
+    <Search />
+  </header>
+);
+};
+
+export default Header;
